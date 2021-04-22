f9c1522e854d44d6c12d6ac1ef9c4f2144a9b906
Максим <Max@MacBook-Pro-Maksim.local>
2021-04-22 20:28:42 +0300

    I want to know PHP

--- strings.php
+++ strings.php
@@ -6,4 +6,20 @@ $text = 'Hello world!';
 $text1='Hello ';
 $text2 = 'world';
 echo $text1 . $text2;
+echo '</br>';
+/*Создайте переменную $age и присвойте ей ваш возраст. 
+Выведите на экран 'My age is {здесь должен быть возраст} years'. 
+Это задания нужно сделать двумя способами. Используя одинарные и двойные кавычки.
+*/
+$age = 25;
+echo 'My age is ' . $age . ' years' ;
+echo '</br>';
+echo "My age is $age years";
+echo '</br>';
+
+$text = 'I';
+$text .=' want';
+$text .=' to know';
+$text .= ' PHP!';
+echo $text;
 ?>
