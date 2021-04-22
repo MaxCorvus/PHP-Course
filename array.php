282f235cd627940e648613111c50412bff0c227b
Максим <Max@MacBook-Pro-Maksim.local>
2021-04-22 20:43:24 +0300

    Task4

--- array.php
+++ array.php
@@ -12,4 +12,10 @@ $arr=['a', 'b', 'c', 'd'];
 echo $arr[0] . '+' . $arr [1];
 echo '</br>';
 echo $arr[2] . '+' . $arr [3];
+echo '</br>';
+$arr=[2, 5, 3, 9];
+$res1= $arr[0]*$arr[1];
+$res2= $arr[2]*$arr[3];
+echo $result = $res1+$res2;
+$res1= $arr[0]*$arr[1];
 ?>
