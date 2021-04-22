967e528f76ee2740688b4d87e4a33a273ad988c9
Максим <Max@MacBook-Pro-Maksim.local>
2021-04-22 20:37:02 +0300

    array_elements

---
+++ array.php
@@ -0,0 +1,12 @@
+<?php
+$arr=['a', 'b', 'c'];
+var_dump($arr);
+echo '</br>';
+echo $arr[0];
+echo '</br>';
+echo $arr[1];
+echo '</br>';
+echo $arr[2];
+echo '</br>';
+$arr=['a', 'b', 'c', 'd'];
+?>
