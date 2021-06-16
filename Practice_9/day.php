98ff3ff2bb413fa1041a4985f2a7a7497a717d5f
MaxCorvus <83407700+MaxCorvus@users.noreply.github.com>
2021-06-16 19:11:32 +0300

    Day_refactoring

--- Practice_9/day.php
+++ Practice_9/day.php
@@ -1,9 +1,26 @@
 <?php
 
+class Day {
+public $dayOfWeek;
+public $fullDate;
+
+   function __construct($day)
+    {
+        $month = date ('m');
+        $year = date ('Y');
+        $currentTimestamp = mktime(0, 0, 0, $month, $day, $year);
+        $this->dayOfWeek = date('l', $currentTimestamp);
+        $this->fullDate = date('d-m-Y', $currentTimestamp);
+    }
+
+    function selectedDay(){
+
+        echo "The day is $this->dayOfWeek - $this->fullDate";
+    }
+    
+
+}
 $day = isset($_GET['dayOfMonth']) ? $_GET['dayOfMonth'] : date('d');
-$month = date ('m');
-$year = date ('Y');
-$currentTimestamp = mktime(0, 0, 0, $month, $day, $year);
-$dayOfWeek = date('l', $currentTimestamp);
-$fullDate = date('d-m-Y', $currentTimestamp);
-echo "The day is $dayOfWeek - $fullDate";
+
+$object = new Day($day);
+$object->selectedDay();
