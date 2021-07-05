af11e9ce60644e52de0e397a511978e871b59b6a
MaxCorvus <83407700+MaxCorvus@users.noreply.github.com>
2021-07-05 18:59:09 +0300

    Eto dich

---
+++ Practice_12/atm.php
@@ -0,0 +1,94 @@
+<?php
+class ATM{
+    public $user;
+    public $pin;
+    private array $userInfos = [
+        1111 => [
+            'name' => 'Vasya',
+            'pin' => 1234,
+            'balance' => 100
+        ],
+        2222 => [
+            'name' => 'Petya',
+            'pin' => 4321,
+            'balance' => 500,
+        ],
+        3333 => [
+            'name' => 'Lena',
+            'pin' => 1212,
+            'balance' => 1000,
+        ],
+    ];
+
+ 
+   public  function isUserExists($user) {
+       $this->user = $user;
+       foreach ($this->userInfos as $id => $value){
+           if ($value['name'] === $user) {
+               return true;
+           }
+            // $usersNames[] = $this->userInfos[$id]['name'];
+       }
+
+       return false;       
+  }
+  public function checkPin($pin){
+    
+    
+    foreach ($this->userInfos as $id=>$value)
+    {   if ($this->user === $value['name']){
+            if ($value['pin'] === $pin)
+            {       $this->pin = $pin;
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            }
+            else {
+                continue;
+            }
+    }
+
+    }
+    public function makeMoney($requiredAmount){
+        foreach ($this->userInfos as $id=>$value){
+        if ($this->user === $value['name']){
+            if ($this->pin === $value['pin']){
+                if ($requiredAmount <= $value['balance']){
+                    echo "У вас осталось " .  $value['balance'] -= $requiredAmount;
+                        return true;
+                }
+                else {
+                    return false;
+                }
+
+            }
+        }
+    }
+}
+
+
+  }
+
+
+$newUser = new ATM;
+
+if ($newUser->isUserExists('Lena')) {
+    if ($newUser->checkPin(1212)){
+        if ($newUser->makeMoney(500)){
+        }
+        else {echo "Недостаточно средств";}
+    }
+    else {
+        echo "Неправильный пинкод!";
+        die;
+    }
+} else {
+    echo " Такого пользователя не существует!";
+    die;
+}
+
+// var_dump($newUser->isUserExists('Vasya'));
+// $newUser->checkPin(123);
+
